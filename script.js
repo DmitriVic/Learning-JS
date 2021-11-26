3c0d8b7ce817b3fb9c845e31af528cbf509cdfd7
DmitriVic <DmitriVic>
2021-11-26 21:11:14 +0300

    26.11.21

--- script.js
+++ script.js
@@ -441,23 +441,13 @@
 
 // 	result = add(multiply(36325, 9824), 777);
 // 	console.log(result);
-let test1 = [1,2,3]
-let test2 = [1,2,3]
 
 
-	let areArraysSame = function (array1,array2){
-		for (let i = 0; i < array1.length; i++) {
-			
-			
-			if (array1[i] !== array2[i]){
-				return false;
-			} else if (array1.length === array2.length){
-				return false;
-			} else {
-				return true;
-			}
-						
-		}
-	}
+	// let areArraysSame = function (array1,array2){
+	// 	for (let i = 0; i < array1.length; i++) {						
+	// 		if (array1[i] !== array2[i] || array1.length !== array2.length ){
+	// 		return false; }										
+	// 	}
+	// 	return true;
+	// }
 
-	areArraysSame(test1, test2);
