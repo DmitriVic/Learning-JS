1a2c86210588d5aaaadae77a1a5a6f9d370bcc8d
DmitriVic <DmitriVic>
2021-11-26 11:14:39 +0300

    26.11.21

--- script.js
+++ script.js
@@ -353,3 +353,41 @@
 	
 //   alert("Вы проиграли " );
 // }
+
+//=========================================					ФУНКЦИИ				===========================================================================================================
+
+
+var pickRandomWord = function (words) {
+	return words[Math.floor(Math.random() * words.length)];
+  };
+
+//   var randomWords = ["Планета", "Червяк", "Цветок", "Компьютер"];
+
+//   pickRandomWord(randomWords);
+
+
+
+// var randomBodyParts = ["глаз", "нос", "череп"];
+// var randomAdjectives = ["вонючая", "унылая", "дурацкая"];
+// var randomWords = ["муха", "выдра", "дубина", "мартышка", "крыса"];
+// // Выбор случайной части тела из массива randomBodyParts:
+// var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
+// // Выбор случайного прилагательного из массива randomAdjectives:
+// var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
+// // Выбор случайного слова из массива randomWords:
+// var randomWord = randomWords[Math.floor(Math.random() * 5)];
+
+// // Соединяем случайные строки в предложение:
+// var randomInsult = "У тебя " + randomBodyPart + " словно " + 
+// randomAdjective + " " + randomWord + "!!!";
+// randomInsult;
+
+var randomBodyParts = ["глаз", "нос", "череп"];
+var randomAdjectives = ["вонючая", "унылая", "дурацкая"];
+var randomWords = ["муха", "выдра", "дубина", "мартышка", "крыса"];
+
+// Соединяем случайные строки в предложение:
+var randomString = "У тебя " + pickRandomWord(randomBodyParts) + 
+" словно " + pickRandomWord(randomAdjectives) + 
+" " + pickRandomWord(randomWords) + "!!!";
+randomString;
